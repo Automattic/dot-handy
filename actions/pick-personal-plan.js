41b91e72d171ee25d915b652447ac77178df8661
James Tien <southp0105@gmail.com>
2021-04-28 23:32:55 +0800

    New action: pick-personal-plan

---
+++ actions/pick-personal-plan.js
@@ -0,0 +1,12 @@
+const { createAction } = require( '../lib/action' );
+
+// Pick the Personal plan at the /plans step
+module.exports = createAction(
+	async ( browser, context, page, extra ) => {
+		// plan step
+		await page.click( 'css=button.is-personal-plan' );
+
+		return {};
+	},
+	'/plans/'
+);

a6211098cf1d323ddaf9de4058e44e4f0ec482d6
James Tien <southp0105@gmail.com>
2021-05-20 10:38:21 +0800

    Added pick-premium-plan action.

---
+++ actions/pick-premium-plan.js
@@ -0,0 +1,12 @@
+const { createAction } = require( '../lib/action' );
+
+// Pick the Premium plan at the /plans step
+module.exports = createAction(
+	async ( browser, context, page, extra ) => {
+		// plan step
+		await page.click( 'css=button.is-premium-plan' );
+
+		return {};
+	},
+	'/plans/'
+);

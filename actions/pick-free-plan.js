4b244693aacb693e1febb83bf504f2aeb3bb5db6
James Tien <southp0105@gmail.com>
2022-04-21 16:22:29 +0800

    Update the pick-free-plan action to adapt the newly introduced plan grid.

--- actions/pick-free-plan.js
+++ actions/pick-free-plan.js
@@ -3,7 +3,8 @@ const { createAction } = require( '../lib/action' );
 // Pick the Free plan at the /plans step
 module.exports = createAction(
 	async ( browser, context, page ) => {
-		await page.click( 'css=#step-header >> css=button[type="button"]' );
+		// Terrible, but it is what it is before a more semantic structure is introduced.
+		await page.click( 'text=Start with Free' );
 
 		return {};
 	},
